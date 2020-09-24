7c8ec21362f16d412857873bd43f1e047cc01b84
Davi Freitas da Silva <davi.frrs@outlook.com>
2020-09-24 17:03:27 -0300

    auth failed #14

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const { User } = require("./models/user");
+const { auth } = require("./middlewares/auth");
 
 const config = require("./config/key");
+const { json } = require("body-parser");
 
 mongoose
   .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,18 +18,53 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.json({ hello: "hoi" });
+app.get("/api/user/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req._id,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+  });
 });
 
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
 
-  user.save((err, userData) => {
+  user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      userData: doc,
+    });
   });
-  return res.status(200).json({
-    success: true,
+});
+
+app.post("/api/user/login", (req, res) => {
+  // 1 - find the email
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth Failed, email not found",
+      });
+    // 2 - Compare the password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "Wrong password, try again",
+        });
+      }
+    });
+    // 3 - generate Token
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res.cookie("x_auth", user.token).status(200).json({
+        loginSuccess: true,
+      });
+    });
   });
 });
 
