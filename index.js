9537e3d850e98b6568c39f4490c9c4857c0ff66c
Davi Freitas da Silva <davi.frrs@outlook.com>
2020-09-24 14:12:14 -0300

    added nodemon and connection to mongo db

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./models/user");
+
 mongoose
   .connect(
-    "mongodb+srv://tekunin:Qwert159@react-blog.qqghu.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    "mongodb+srv://tekunin:Qwert159@react-blog.qqghu.mongodb.net/React-blog?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("DB connected"))
   .catch((err) => console.error(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.json({ hello: "hoi" });
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+  });
+  return res.status(200).json({
+    success: true,
+  });
 });
 
 app.listen(5000);
